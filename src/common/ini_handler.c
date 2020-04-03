e910ad251ff253d840d4abddab83697519e4669a
Michal Rudolf <michal.rudolf23@gmail.com>
2020-04-03 11:02:04 +0200

    create ini_load solution for WUI

--- src/common/ini_handler.c
+++ src/common/ini_handler.c
@@ -2,8 +2,14 @@
 
 #include "ini_handler.h"
 #include "ff.h"
+#include "eeprom.h"
+#include "lwip/dhcp.h"
+#include "lwip/netifapi.h"
+#include "dns.h"
 
 static const char network_ini_file_name[] = "/lan_settings.ini"; //change -> change msgboxes in screen_lan_settings
+
+static int ini_config_handler(void *user, const char *section, const char *name, const char *value);
 
 uint8_t ini_save_file(const char *ini_save_str) {
 
@@ -37,6 +43,148 @@ uint8_t ini_load_file(ini_handler handler, void *user_struct) {
 
     if (ini_parse_string(ini_file_str, handler, user_struct) < 0) {
         return 0;
+    }
+    return 1;
+}
+
+static void _change_any_to_static(_networkconfig_t * config) {
+    if (netif_is_up(&eth0)) {
+        netifapi_netif_set_down(&eth0);
+    }
+    config->lan_flag |= LAN_EEFLG_TYPE;
+    eeprom_set_var(EEVAR_LAN_FLAG, variant8_ui8(config->lan_flag));
+
+    netifapi_netif_set_addr(&eth0,
+        (const ip4_addr_t *)&(config->lan_ip4_addr),
+        (const ip4_addr_t *)&(config->lan_ip4_msk),
+        (const ip4_addr_t *)&(config->lan_ip4_gw)
+        );
+
+    dns_setserver(0, (const ip4_addr_t *)&(config->dns1_ip4));
+    dns_setserver(1, (const ip4_addr_t *)&(config->dns2_ip4));
+    if (netif_is_link_up(&eth0) && !(config->lan_flag & LAN_EEFLG_ONOFF)) {
+        netifapi_netif_set_up(&eth0);
+    }
+}
+
+static void _change_static_to_dhcp(uint8_t lan_flg) {
+    if (netif_is_up(&eth0)) {
+        netifapi_netif_set_down(&eth0);
+    }
+    lan_flg &= ~LAN_EEFLG_TYPE;
+    eeprom_set_var(EEVAR_LAN_FLAG, variant8_ui8(lan_flg));
+    if (netif_is_link_up(&eth0) && !(lan_flg & LAN_EEFLG_ONOFF)) {
+        netifapi_netif_set_up(&eth0);
+    }
+}
+
+uint8_t load_config_from_ini(void){
+
+    _networkconfig_t tmp_config;
+    tmp_config.lan_flag = eeprom_get_var(EEVAR_LAN_FLAG).ui8;
+    tmp_config.set_flag = tmp_config.dns1_ip4.addr = tmp_config.dns2_ip4.addr = 0;
+
+    if (ini_load_file(ini_config_handler, &tmp_config) == 0) {
+        return 0;
+    }
+
+    // type=STATIC/DHCP is in INI file
+    if (tmp_config.set_flag & _NETVAR_MSK(_NETVAR_LAN_FLAGS)) {
+        // if lan type is set to STATIC
+        if ((tmp_config.lan_flag & LAN_EEFLG_TYPE)){
+            if ((tmp_config.set_flag & _NETVAR_STATIC_LAN_ADDRS) != _NETVAR_STATIC_LAN_ADDRS ||
+                ((tmp_config.set_flag & _NETVAR_MSK(_NETVAR_DNS1)) == 0 && 
+                (tmp_config.set_flag & _NETVAR_MSK(_NETVAR_DNS2)) == 0 ) || 
+                (tmp_config.dns1_ip4.addr == 0 && tmp_config.dns2_ip4.addr == 0)) {
+                return 0;
+            }
+            eeprom_set_var(EEVAR_LAN_IP4_ADDR, variant8_ui32(tmp_config.lan_ip4_addr.addr));
+            eeprom_set_var(EEVAR_LAN_IP4_MSK, variant8_ui32(tmp_config.lan_ip4_msk.addr));
+            eeprom_set_var(EEVAR_LAN_IP4_GW, variant8_ui32(tmp_config.lan_ip4_gw.addr));
+            eeprom_set_var(EEVAR_LAN_IP4_DNS1, variant8_ui32(tmp_config.dns1_ip4.addr));
+            eeprom_set_var(EEVAR_LAN_IP4_DNS2, variant8_ui32(tmp_config.dns2_ip4.addr));
+        }
+    }
+    if (tmp_config.set_flag & _NETVAR_MSK(_NETVAR_HOSTNAME)) {
+        strlcpy(interface_hostname, tmp_config.hostname, LAN_HOSTNAME_MAX_LEN + 1);
+        eth0.hostname = interface_hostname;
+        variant8_t hostname = variant8_pchar(tmp_config.hostname, 0, 0);
+        eeprom_set_var(EEVAR_LAN_HOSTNAME, hostname);
+        //variant8_done() is not called, variant_pchar with init flag 0 doesnt hold its memory
+    }
+#ifdef BUDDY_ENABLE_CONNECT
+    if (tmp_config.set_flag & _NETVAR_MSK(_NETVAR_CONNECT_TOKEN)) {
+        variant8_t token = variant8_pchar(tmp_config.connect_token, 0, 0);
+        eeprom_set_var(EEVAR_CONNECT_TOKEN, token);
+        //variant8_done() is not called, variant_pchar with init flag 0 doesnt hold its memory
+    }
+    if (tmp_config.set_flag & _NETVAR_MSK(_NETVAR_CONNECT_IP4)) {
+        eeprom_set_var(EEVAR_CONNECT_IP4, variant8_ui32(tmp_config.connect_ip4.addr));
+    }
+#endif // BUDDY_ENABLE_CONNECT
+
+    // type=STATIC/DHCP is in INI file    
+    if (tmp_config.set_flag & _NETVAR_MSK(_NETVAR_LAN_FLAGS)) {
+        // if there is change from STATIC to DHCP
+        if (eeprom_get_var(EEVAR_LAN_FLAG).ui8 & _NETVAR_MSK(_NETVAR_LAN_FLAGS) && 
+            !(tmp_config.lan_flag & LAN_EEFLG_TYPE)) {
+            _change_static_to_dhcp(tmp_config.lan_flag);
+        } else if (tmp_config.lan_flag & LAN_EEFLG_TYPE){
+            _change_any_to_static(&tmp_config);
+        }
+    }
+    return 1;
+}
+
+static int ini_config_handler(void *user, const char *section, const char *name, const char *value) {
+    _networkconfig_t *tmp_config = (_networkconfig_t *)user;
+#define MATCH(s, n) strcmp(section, s) == 0 && strcmp(name, n) == 0
+    if (MATCH("lan_ip4", "type")) {
+        if (strncmp(value, "DHCP", 4) == 0 || strncmp(value, "dhcp", 4) == 0) {
+            tmp_config->lan_flag &= ~LAN_EEFLG_TYPE;
+            tmp_config->set_flag |= _NETVAR_MSK(_NETVAR_LAN_FLAGS);
+        } else if (strncmp(value, "STATIC", 6) == 0 || strncmp(value, "static", 6) == 0) {
+            tmp_config->lan_flag |= LAN_EEFLG_TYPE;
+            tmp_config->set_flag |= _NETVAR_MSK(_NETVAR_LAN_FLAGS);
+        }
+    } else if (MATCH("lan_ip4", "hostname")) {
+        strlcpy(tmp_config->hostname, value, LAN_HOSTNAME_MAX_LEN + 1);
+        tmp_config->hostname[LAN_HOSTNAME_MAX_LEN] = '\0';
+        tmp_config->set_flag |= _NETVAR_MSK(_NETVAR_HOSTNAME);
+    } else if (MATCH("lan_ip4", "address")) {
+        if (ip4addr_aton(value, &tmp_config->lan_ip4_addr)) {
+            tmp_config->set_flag |= _NETVAR_MSK(_NETVAR_LAN_IP4_ADDR);
+        }
+    } else if (MATCH("lan_ip4", "mask")) {
+        if (ip4addr_aton(value, &tmp_config->lan_ip4_msk)) {
+            tmp_config->set_flag |= _NETVAR_MSK(_NETVAR_LAN_IP4_MSK);
+        }
+    } else if (MATCH("lan_ip4", "gateway")) {
+        if (ip4addr_aton(value, &tmp_config->lan_ip4_gw)) {
+            tmp_config->set_flag |= _NETVAR_MSK(_NETVAR_LAN_IP4_GW);
+        }
+    } else if (MATCH("lan_ip4", "dns1")) {
+        if (ip4addr_aton(value, &tmp_config->dns1_ip4)) {
+            tmp_config->set_flag |= _NETVAR_MSK(_NETVAR_DNS1);
+        }
+    } else if (MATCH("lan_ip4", "dns2")) {
+        if (ip4addr_aton(value, &tmp_config->dns2_ip4)) {
+            tmp_config->set_flag |= _NETVAR_MSK(_NETVAR_DNS2);
+        }
+    }
+#ifdef BUDDY_ENABLE_CONNECT
+    else if (MATCH("connect", "address")) {
+        if (ip4addr_aton(value, &tmp_config->connect_ip4)) {
+            tmp_config->set_flag |= _NETVAR_MSK(_NETVAR_CONNECT_IP4);
+        }
+    } else if (MATCH("connect", "token")) {
+        strlcpy(tmp_config->connect_token, value, CONNECT_TOKEN_SIZE + 1);
+        tmp_config->connect_token[CONNECT_TOKEN_SIZE] = '\0';
+        tmp_config->set_flag |= _NETVAR_MSK(_NETVAR_CONNECT_TOKEN);
+    }
+#endif // BUDDY_ENABLE_CONNECT
+    else {
+        return 0; /* unknown section/name, error */
     }
     return 1;
 }
--- src/common/ini_handler.h
+++ src/common/ini_handler.h
@@ -4,11 +4,42 @@
 
 #include <stdint.h>
 #include "ini.h"
+#include "lwip.h"
 #define MAX_INI_SIZE 200
 
 uint8_t ini_save_file(const char *ini_save_str);
 uint8_t ini_load_file(ini_handler handler, void *user_struct);
 
 char ini_file_str[MAX_INI_SIZE];
+
+typedef enum{
+    _NETVAR_LAN_FLAGS,
+    _NETVAR_HOSTNAME,
+    _NETVAR_CONNECT_TOKEN,
+    _NETVAR_LAN_IP4_ADDR,
+    _NETVAR_LAN_IP4_MSK,
+    _NETVAR_LAN_IP4_GW,
+    _NETVAR_DNS1,
+    _NETVAR_DNS2,
+    _NETVAR_CONNECT_IP4,
+} _NETVAR_t;
+#define _NETVAR_MSK(n_id) ((uint16_t)1 << (n_id))
+#define _NETVAR_STATIC_LAN_ADDRS \
+    (_NETVAR_MSK(_NETVAR_LAN_IP4_ADDR) | _NETVAR_MSK(_NETVAR_LAN_IP4_MSK) | _NETVAR_MSK(_NETVAR_LAN_IP4_GW))
+
+typedef struct {
+    uint8_t lan_flag;
+    char hostname[LAN_HOSTNAME_MAX_LEN + 1];
+#ifdef BUDDY_ENABLE_CONNECT
+    char connect_token[CONNECT_TOKEN_SIZE + 1];
+    ip4_addr_t connect_ip4;
+#endif // BUDDY_ENABLE_CONNECT
+    ip4_addr_t lan_ip4_addr;
+    ip4_addr_t lan_ip4_msk;
+    ip4_addr_t lan_ip4_gw;
+    uint16_t set_flag;
+    ip4_addr_t dns1_ip4;
+    ip4_addr_t dns2_ip4;
+} _networkconfig_t;
 
 #endif //INI_HANDLER_H
